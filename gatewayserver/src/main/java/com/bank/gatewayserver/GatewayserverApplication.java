4c8950ba1ee8607f9bf742ec8bc96131a0c02868
Przemyslaw Pukala <przemyslaw.pukala@asseco.pl>
2021-10-23 13:05:15 +0200

    Added routing

--- gatewayserver/src/main/java/com/bank/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/bank/gatewayserver/GatewayserverApplication.java
@@ -2,7 +2,12 @@ package com.bank.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Date;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -12,4 +17,23 @@ public class GatewayserverApplication {
 		SpringApplication.run(GatewayserverApplication.class, args);
 	}
 
+	@Bean
+	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(p -> p
+						.path("/bank/accounts/**")
+						.filters(f -> f.rewritePath("/bank/accounts/(?<segment>.*)", "/${segment}")
+								.addResponseHeader("X-Response-Time", new Date().toString()))
+						.uri("lb://ACCOUNTS"))
+				.route(p -> p
+						.path("/bank/loans/**")
+						.filters(f -> f.rewritePath("/bank/loans/(?<segment>.*)", "/${segment}")
+								.addResponseHeader("X-Response-Time", new Date().toString()))
+						.uri("lb://LOANS"))
+				.route(p -> p
+						.path("/bank/cards/**")
+						.filters(f -> f.rewritePath("/bank/cards/(?<segment>.*)", "/${segment}")
+								.addResponseHeader("X-Response-Time", new Date().toString()))
+						.uri("lb://CARDS")).build();
+	}
 }
